703a65e13f179e2cd9066cea9712d5bbdb2dbd52
adityatodkar1 <adityatodkar80@gmail.com>
2026-05-30 13:37:19 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,349 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const resList = [
+  {
+    "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+    info: {
+      id: "568722",
+      name: "Pizza Hut",
+      cloudinaryImageId:
+        "RX_THUMBNAIL/IMAGES/VENDOR/2026/2/12/318566ef-e68c-4827-9111-8e1f90e9ab06_568722.JPG",
+      locality: "Sector 17",
+      areaName: "Chinchwad",
+      costForTwo: "₹350 for two",
+      cuisines: ["Pizzas"],
+      avgRating: 4.2,
+      parentId: "721",
+      avgRatingString: "4.2",
+      totalRatingsString: "3.4K+",
+      sla: {
+        deliveryTime: 18,
+        lastMileTravel: 0.6,
+        serviceability: "SERVICEABLE",
+        slaString: "15-20 mins",
+        lastMileTravelString: "0.6 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2026-05-29 23:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "FREE ITEM",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+    analytics: {
+      context: "seo-data-79e6c72d-9254-4d01-9399-af0a8de4f7a1",
+    },
+    cta: {
+      link: "https://www.swiggy.com/city/pune/pizza-hut-sector-17-chinchwad-rest568722",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+    info: {
+      id: "617985",
+      name: "Chinese Wok",
+      cloudinaryImageId: "e0839ff574213e6f35b3899ebf1fc597",
+      locality: "Ewing",
+      areaName: "Talawade",
+      costForTwo: "₹250 for two",
+      cuisines: ["Chinese", "Asian", "Tibetan", "Desserts"],
+      avgRating: 4.1,
+      parentId: "61955",
+      avgRatingString: "4.1",
+      totalRatingsString: "4.0K+",
+      sla: {
+        deliveryTime: 15,
+        lastMileTravel: 0.8,
+        serviceability: "SERVICEABLE",
+        slaString: "15-20 mins",
+        lastMileTravelString: "0.8 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2026-05-29 23:00:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "android/static-assets/icons/big_rx.png",
+            description: "bolt!",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "bolt!",
+                  imageId: "android/static-assets/icons/big_rx.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "70% OFF",
+        subHeader: "UPTO ₹140",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+    analytics: {
+      context: "seo-data-79e6c72d-9254-4d01-9399-af0a8de4f7a1",
+    },
+    cta: {
+      link: "https://www.swiggy.com/city/pune/chinese-wok-ewing-talawade-rest617985",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+  {
+    "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+    info: {
+      id: "584695",
+      name: "Domino's Pizza",
+      cloudinaryImageId:
+        "RX_THUMBNAIL/IMAGES/VENDOR/2026/3/27/675d9d21-cfbf-4cd5-81b4-d649ffa04463_584695.JPG",
+      locality: "Square Chikali",
+      areaName: "Nigdi",
+      costForTwo: "₹400 for two",
+      cuisines: ["Pizzas", "Italian", "Pastas", "Desserts"],
+      avgRating: 4.1,
+      parentId: "2456",
+      avgRatingString: "4.1",
+      totalRatingsString: "2.3K+",
+      sla: {
+        deliveryTime: 25,
+        lastMileTravel: 1.7,
+        serviceability: "SERVICEABLE",
+        slaString: "20-25 mins",
+        lastMileTravelString: "1.7 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2026-05-29 23:59:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "brand_cards/Badges%202026/40_Best%20in%20Bolt2026.png",
+            description: "Best in Bolt",
+          },
+          {
+            imageId: "brand_cards/Badges%202026/72_Best%20in%20Pizza2026.png",
+            description: "Best in Pizza",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "Best in Bolt",
+                  imageId:
+                    "brand_cards/Badges%202026/40_Best%20in%20Bolt2026.png",
+                  theme: "",
+                },
+              },
+              {
+                attributes: {
+                  description: "Best in Pizza",
+                  imageId:
+                    "brand_cards/Badges%202026/72_Best%20in%20Pizza2026.png",
+                  theme: "",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "ITEMS",
+        subHeader: "AT ₹59",
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {},
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+    analytics: {
+      context: "seo-data-79e6c72d-9254-4d01-9399-af0a8de4f7a1",
+    },
+    cta: {
+      link: "https://www.swiggy.com/city/pune/dominos-pizza-square-chikali-nigdi-rest584695",
+      text: "RESTAURANT_MENU",
+      type: "WEBLINK",
+    },
+    widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+  },
+];
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://static.vecteezy.com/system/resources/thumbnails/035/767/491/small/food-logo-silhouette-black-color-illustration-vector.jpg"
+        ></img>
+      </div>
+      <div>
+        <div className="nav-items">
+          <ul>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Conatct us</li>
+            <li>Cart</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const ResaturantCard = (props) => {
+  const { resData } = props;
+  const { name, cuisines, avgRating, sla } = resData.info;
+  return (
+    <div className="res-card">
+      <img
+        className="res-logo"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/FOOD_CATALOG/IMAGES/CMS/2024/3/27/1364ea32-80e6-4b37-964a-2f019fd3c51e_69c3766c-1b95-4e60-a51e-172293a52679.png_compressed"
+      ></img>
+      <h3>{name}</h3>
+      <h4>{cuisines.join(",")}</h4>
+      <h5>{avgRating}</h5>
+      <h6>{sla.deliveryTime} Min</h6>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        {/* <ResaturantCard resData = {resList[0]}/>
+        <ResaturantCard resData = {resList[1]}/>
+         <ResaturantCard resData = {resList[2]}/> */}
+        {resList.map((restaurant) => {
+          return (
+            <ResaturantCard key={restaurant.info.id} resData={restaurant} />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+const Footer = () => {
+  return <div>Footer</div>;
+};
+
+const AppLayout = () => {
+  return (
+    <div>
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
