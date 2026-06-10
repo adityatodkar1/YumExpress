9a9d4780d9d37924b3d4cc322a7724b350cfeb5a
adityatodkar1 <adityatodkar80@gmail.com>
2026-06-10 10:19:06 +0530

    API commit

--- src/components/Body.js
+++ src/components/Body.js
@@ -19,7 +19,7 @@ const Body = () => {
     //   "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.52110&lng=73.85020&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING",
     // );
     const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.52110&lng=73.85020&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING",
+      "https://demodata-76qi.onrender.com/api/restaurants/",
     );
     const json = await data.json();
     // console.log(json);
--- src/utils/useRestaurantMenu.js
+++ src/utils/useRestaurantMenu.js
@@ -17,9 +17,7 @@ const useRestaurantMenu = () => {
     // );
 
     const data = await fetch(
-      "https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.52110&lng=73.85020&restaurantId=" +
-        resId,
-    );
+      "https://demomenu.onrender.com/api/menu/" + resId );
 
     const json = await data.json();
     setResMenu(json.data);
